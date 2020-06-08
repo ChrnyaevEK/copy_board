ff8bb7bfc4bec7c8c5b4a711bea8b45b9fab0baa
cherniaev egor <chrnyaevek@gmail.com>
2020-06-08 17:14:55 +0200

    add tool

--- js/gui.js
+++ js/gui.js
@@ -2,4 +2,6 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
     $('.modal').modal();
     $('.datepicker').datepicker();
+    $('.tooltipped').tooltip();
+    $('select').formSelect();
 });
